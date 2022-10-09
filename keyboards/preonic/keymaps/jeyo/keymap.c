529fffa20023c4c0159b42a9bf8a465a325dbcfc
Jeyo Programmer <jeyo.programmer@gmail.com>
2022-10-09 20:58:40 +0800

    cleanup of keymap.c

--- keyboards/preonic/keymaps/jeyo/keymap.c
+++ keyboards/preonic/keymaps/jeyo/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_MAIN] = LAYOUT_preonic_grid( // TODO: copy from jj40 and ergodox
+[_MAIN] = LAYOUT_preonic_grid(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   JC_L1LF, MO_CURS, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    MO_MOUS, JC_L2RG,
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LGUI, _______, _______, _______, JC_SHSP, KC_BSPC, JC_ESCC, KC_ENT,  ADJUST,  DF_GAME, _______, KC_RGUI
 ),
 
-[_NUMBER] = LAYOUT_preonic_grid( // TODO: mouse mode
+[_NUMBER] = LAYOUT_preonic_grid(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_PAST, KC_F12,
   _______, _______, _______, _______, _______, KC_EQL,  KC_MINS, KC_4,    KC_5,    KC_6,    KC_PSLS, _______,
@@ -77,28 +77,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, KC_PDOT, KC_0,    KC_PMNS, _______
 ),
 
-[_MOUSE] = LAYOUT_preonic_grid( // TODO: num mode
+[_MOUSE] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, KC_MS_U, _______, _______, _______, _______, KC_WH_U, _______, _______, _______,
   _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, KC_BTN3, KC_BTN1, KC_BTN2, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______
 ),
-[_CURSOR] = LAYOUT_preonic_grid( // TODO: num mode
+
+[_CURSOR] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______,  _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
-[_GAME] = LAYOUT_preonic_grid( // TODO: num mode
+
+[_GAME] = LAYOUT_preonic_grid(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   KC_LCTL, KC_LALT, KC_LGUI, _______, KC_BSPC, KC_SPC,  KC_ESC,  KC_ENT,  _______, DF_GAME, KC_RGUI, KC_RCTL
 ),
-[_ADJUST] = LAYOUT_preonic_grid( // TODO: num mode
+
+[_ADJUST] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, EEP_RST, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, _______, _______, _______, _______, _______,
@@ -130,80 +133,3 @@ bool music_mask_user(uint16_t keycode) {
       return true;
   }
 }
-
-// bool muse_mode = false;
-// uint8_t last_muse_note = 0;
-// uint16_t muse_counter = 0;
-// uint8_t muse_offset = 70;
-// uint16_t muse_tempo = 50;
-
-// // TODO: to remove
-// bool encoder_update_user(uint8_t index, bool clockwise) {
-//   if (muse_mode) {
-//     if (IS_LAYER_ON(_RAISE)) {
-//       if (clockwise) {
-//         muse_offset++;
-//       } else {
-//         muse_offset--;
-//       }
-//     } else {
-//       if (clockwise) {
-//         muse_tempo+=1;
-//       } else {
-//         muse_tempo-=1;
-//       }
-//     }
-//   } else {
-//     if (clockwise) {
-//       register_code(KC_PGDN);
-//       unregister_code(KC_PGDN);
-//     } else {
-//       register_code(KC_PGUP);
-//       unregister_code(KC_PGUP);
-//     }
-//   }
-//     return true;
-// }
-
-// // TODO: to remove
-// bool dip_switch_update_user(uint8_t index, bool active) {
-//     switch (index) {
-//         case 0:
-//             if (active) {
-//                 layer_on(_ADJUST);
-//             } else {
-//                 layer_off(_ADJUST);
-//             }
-//             break;
-//         case 1:
-//             if (active) {
-//                 muse_mode = true;
-//             } else {
-//                 muse_mode = false;
-//             }
-//     }
-//     return true;
-// }
-
-
-// void matrix_scan_user(void) {
-// #ifdef AUDIO_ENABLE
-//     if (muse_mode) {
-//         if (muse_counter == 0) {
-//             uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-//             if (muse_note != last_muse_note) {
-//                 stop_note(compute_freq_for_midi_note(last_muse_note));
-//                 play_note(compute_freq_for_midi_note(muse_note), 0xF);
-//                 last_muse_note = muse_note;
-//             }
-//         }
-//         muse_counter = (muse_counter + 1) % muse_tempo;
-//     } else {
-//         if (muse_counter) {
-//             stop_all_notes();
-//             muse_counter = 0;
-//         }
-//     }
-// #endif
-// }
-
