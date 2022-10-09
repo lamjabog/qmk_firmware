a2d651945e16f310f6f31b096b493b70c3122f86
Jeyo Programmer <jeyo.programmer@gmail.com>
2022-10-09 20:55:59 +0800

    removed bootmagic lite

--- keyboards/preonic/keymaps/jeyo/rules.mk
+++ keyboards/preonic/keymaps/jeyo/rules.mk
@@ -1,6 +1,7 @@
-SRC += muse.c
-
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 ENCODER_ENABLE = no
 DIP_SWITCH_ENABLE = no
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+
+SRC += muse.c
