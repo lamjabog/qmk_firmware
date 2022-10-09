489065e876b9cfd0f652c9de592cda1a361fa297
Jeyo Programmer <jeyo.programmer@gmail.com>
2022-10-09 20:56:34 +0800

    Added adjust layout

--- keyboards/preonic/keymaps/jeyo/keymap.c
+++ keyboards/preonic/keymaps/jeyo/keymap.c
@@ -34,7 +34,8 @@ enum preonic_layers {
   _NUMBER,
   _MOUSE,
   _CURSOR,
-  _GAME
+  _GAME,
+  _ADJUST
 };
 
 enum preonic_keycodes {
@@ -42,6 +43,7 @@ enum preonic_keycodes {
   NUMBER,
   MOUSE,
   CURSOR,
+  ADJUST
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -64,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   JC_L1LF, MO_CURS, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    MO_MOUS, JC_L2RG,
   KC_LSPO, JC_CTRZ, JC_ALTX, KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, JC_ALDT, JC_CTRS, KC_RSPC,
-  KC_LGUI, _______, _______, _______, JC_SHSP, KC_BSPC, JC_ESCC, KC_ENT,  _______, DF_GAME, _______, KC_RGUI
+  KC_LGUI, _______, _______, _______, JC_SHSP, KC_BSPC, JC_ESCC, KC_ENT,  ADJUST,  DF_GAME, _______, KC_RGUI
 ),
 
 [_NUMBER] = LAYOUT_preonic_grid( // TODO: mouse mode
@@ -84,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_CURSOR] = LAYOUT_preonic_grid( // TODO: num mode
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  EE_CLR,  _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______,
+  _______,  _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
@@ -96,73 +98,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   KC_LCTL, KC_LALT, KC_LGUI, _______, KC_BSPC, KC_SPC,  KC_ESC,  KC_ENT,  _______, DF_GAME, KC_RGUI, KC_RCTL
 ),
+[_ADJUST] = LAYOUT_preonic_grid( // TODO: num mode
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, EEP_RST, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
 
 };
 
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  // switch (keycode) {
-  //       case QWERTY:
-  //         if (record->event.pressed) {
-  //           set_single_persistent_default_layer(_QWERTY);
-  //         }
-  //         return false;
-  //         break;
-  //       case COLEMAK: // TODO: to remove
-  //         if (record->event.pressed) {
-  //           set_single_persistent_default_layer(_COLEMAK);
-  //         }
-  //         return false;
-  //         break;
-  //       case DVORAK: // TODO: to remove
-  //         if (record->event.pressed) {
-  //           set_single_persistent_default_layer(_DVORAK);
-  //         }
-  //         return false;
-  //         break;
-  //       case LOWER:
-  //         if (record->event.pressed) {
-  //           layer_on(_LOWER);
-  //           update_tri_layer(_LOWER, _RAISE, _ADJUST);
-  //         } else {
-  //           layer_off(_LOWER);
-  //           update_tri_layer(_LOWER, _RAISE, _ADJUST);
-  //         }
-  //         return false;
-  //         break;
-  //       case RAISE:
-  //         if (record->event.pressed) {
-  //           layer_on(_RAISE);
-  //           update_tri_layer(_LOWER, _RAISE, _ADJUST);
-  //         } else {
-  //           layer_off(_RAISE);
-  //           update_tri_layer(_LOWER, _RAISE, _ADJUST);
-  //         }
-  //         return false;
-  //         break;
-  //       // TODO: add case for cursor, game and fg
-  //       case BACKLIT:
-  //         if (record->event.pressed) {
-  //           register_code(KC_RSFT);
-  //           #ifdef BACKLIGHT_ENABLE
-  //             backlight_step();
-  //           #endif
-  //           #ifdef RGBLIGHT_ENABLE
-  //             rgblight_step();
-  //           #endif
-  //           #ifdef __AVR__
-  //           writePinLow(E6);
-  //           #endif
-  //         } else {
-  //           unregister_code(KC_RSFT);
-  //           #ifdef __AVR__
-  //           writePinHigh(E6);
-  //           #endif
-  //         }
-  //         return false;
-  //         break;
-  //     }
-  //   return true;
-// };
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+        case ADJUST:
+          if (record->event.pressed) {
+            layer_on(_ADJUST);
+          } else {
+            layer_off(_ADJUST);
+          }
+          return false;
+          break;
+      }
+    return true;
+};
+
+bool music_mask_user(uint16_t keycode) {
+  switch (keycode) {
+    case ADJUST:
+      return false;
+    default:
+      return true;
+  }
+}
 
 // bool muse_mode = false;
 // uint8_t last_muse_note = 0;
@@ -240,12 +207,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // #endif
 // }
 
-// bool music_mask_user(uint16_t keycode) {
-//   switch (keycode) {
-//     case RAISE:
-//     case LOWER:
-//       return false;
-//     default:
-//       return true;
-//   }
-// }
